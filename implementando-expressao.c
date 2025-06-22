471d7b3b38ed9bf40cbf5801a665c594503e6139
Matheus Castelo <matheuscastelo.tech@gmail.com>
2025-06-22 14:47:34 -0300

    add: expressao em C para implementar em Assembly

---
+++ implementando-expressao.c
@@ -0,0 +1,8 @@
+int main(){
+    register int num1 = 30;
+    register int num2 = 70;
+    register int num3 = 99;
+    int soma = num1 + num2;
+    int soma = ((num1+num2) - num3)+num1;
+    return 0;
+}
