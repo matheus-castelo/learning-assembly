354c893fe525ad0d0f561d591918e5f576fe0661
Matheus Castelo <matheuscastelo.tech@gmail.com>
2025-06-22 14:40:51 -0300

    tudo vira binario

---
+++ tudovirabinario.asm
@@ -0,0 +1,8 @@
+SECTION .data
+decimal DB 101 ; SEM NOTAÇÃO
+hexadecimalPosFixa DB 0xAF ; Hexadecimal usa notação pós-fixa  (h) ou pré-fixa (x)
+hexadecimalPreFixa DB 0AFh; "h" vem de hexa. 0 é uma boa prática.
+binarioPosFixo DB 10101111b ; Notação pós-fixa (b) 
+binarioPreFixo DB 0b10101111 ; Notação pré-fixa (b)
+octalPosFixo DB 777o
+octalPreFixo DB 0o777 
